1cfe5ec566c08a6d759c851fca798f3deeac8e84
Jozef Lang <jozef@Jozef-NTB.(none)>
2012-12-24 02:07:27 +0100

    Project skeleton

---
+++ src/com/jozeflang/android/germanirregularverbs/main/EntryPointActivity.java
@@ -0,0 +1,17 @@
+package com.jozeflang.android.germanirregularverbs.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/** 
+ * @author Jozef Lang (developer@jozeflang.com)
+ */
+public class EntryPointActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.linear_layout);
+	}
+
+}
